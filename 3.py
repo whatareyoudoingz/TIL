10e9adc8f2c830be609808b2b3c6d3e55760c30e
whatareyoudoingz <wlsdk3165@icloud.com>
2023-02-02 11:55:01 +0900

    new

--- 3.py
+++ 3.py
@@ -1,2 +1,16 @@
 import sys
 sys.stdin = open("input_1.txt", "r")
+
+m,n=7, 7
+
+## 리스트 초기화
+
+graph=[[] for _ in range(n)] 
+
+## 연결
+for _ in range(m):
+    v1,v2=map(int,input().split())
+    graph[v1].append(v2)
+    graph[v2].append(v1)
+
+print(graph)
